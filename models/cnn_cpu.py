53133e9a182df0a238586208524c67e2baa7f970
Zach Shay <zach@zshay.io>
2025-02-04 09:34:03 -0500

    Enhance: create skeleton code

--- models/cnn_cpu.py
+++ models/cnn_cpu.py
@@ -1,1 +1,15 @@
-# stub code
+import torch
+import torch.nn as nn
+from scripts.utils import load_data, train_model
+
+class CNN(nn.module):
+    # ... (CNN layers) ...
+    pass
+
+device = torch.device("cpu")
+
+train_loader, test_loader = load_data(batch_size = 32)
+
+model = CNN().to(device)
+
+# train_model(model, device, train_loader, optimizer, epochs = 10)
