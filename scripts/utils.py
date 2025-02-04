97c272c063808d92dcbca81f24df941946c5af81
Zach Shay <zach@zshay.io>
2025-02-04 09:19:33 -0500

    Enhance: add function stubs to utils.py

---
+++ scripts/utils.py
@@ -0,0 +1,10 @@
+import torch
+from touchvision import datasets, transforms
+
+def load_data(batch_size):
+    """common program to load & transform data (MNIST)"""
+    return
+
+def train_model(model, device, train_loader, optimizer, epochs):
+    """common program to execute training loop"""
+    return
